c069f1c65d895bfdcd4396f84d52ab5d782aa912
mohosny14 <mohosny14@gmail.com>
2022-10-01 17:22:08 +0200

    Add project files.

---
+++ Core/Entities/Owner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class Owner : EntityBase
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Berif { get; set; }
+        public string Image { get; set; }
+        public Adress Adress { get; set; }
+    }
+}
